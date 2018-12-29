ae03effe10477b116903a26b03dfd6b23f252427
剛史 瀧口 <Takeshi.Takiguchi0403@outlook.com>
2018-12-29 15:58:45 +0900

    change

--- RasPiLChika.py
+++ RasPiLChika.py
@@ -1,11 +1,11 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BCM)  # BCM:GPIO論理番号指定
+GPIO.setmode(GPIO.BCM) 
 GPIO.setup(2,GPIO.OUT)
 for iLoop in range(3):
-    GPIO.output(2,True)     # GPIO2←ON
+    GPIO.output(2,True)   
     time.sleep(2)
-    GPIO.output(2,False)    # GPIO2←OFF
+    GPIO.output(2,False)   
     time.sleep(2)
 GPIO.cleanup()
